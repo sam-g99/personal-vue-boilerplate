7b42495b3b14c8e19d8b8086bb9cc0edfb9b16b9
Sam G <samuelgraham@protonmail.com>
2019-10-02 09:06:49 -0400

    added vuex and store state examples

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,6 +3,8 @@
 		<div>
 			<h1>Sam's Vue Boilerplate</h1>
 			<p>Airbnb style guide</p>
+			<p>{{ this.$store.state.hello }}</p>
+			<p>{{ mapped }}</p>
 			<router-link to="page2"> Page 2 </router-link> <br />
 			<a
 				href="https://github.com/sam-g99/personal-vue-boilerplate"
@@ -16,7 +18,13 @@
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex';
+
+export default {
+	computed: {
+		...mapState(['mapped']),
+	},
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/store.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+	state: {
+		hello: 'Hey I am a state from the Vuex store!',
+		mapped: 'Hey I am a mapped state from the vuex store!',
+	},
+});
+
+export default store;
